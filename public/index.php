4948f4a0a1ead49d824751d498891da8b550febc
Jaimeson Waugh <jaimeson@4cesi.com>
2019-05-06 15:48:47 -0700

    Create basic complete version

--- public/index.php
+++ public/index.php
@@ -1,10 +1,2 @@
-<!DOCTYPE html>
-<head>
-</head>
-<body>
-<form action="../src/index.php" method="post">
-    <input type="text" name="url">
-    <input type="submit" value="Submit">
-</form>
-</body>
-</html>
+<?php
+    require_once( '../src/index.php' );
--- src/index.php
+++ src/index.php
@@ -1,24 +1,15 @@
 <?php
 
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
+    // Turn off all errors.
+    ini_set( 'display_errors', 0 );
+    ini_set( 'display_startup_errors', 0 );
+    error_reporting( E_NONE );
 
     require_once( 'vendor/autoload.php' );
+	use Enrise\Uri;
     use WaughJ\WebpageLinksList\WebpageLinksList;
     use WaughJ\ImageAltTag\ImageAltTagList;
-
-    $url = $_POST[ 'url' ];
-    $links_list = new WebpageLinksList( $url );
-    $links_data = $links_list->getData();
-
-    $websites = [];
-    foreach ( $links_data as $url => $data )
-    {
-        $alts = new ImageAltTagList( $data->raw_body );
-        $alts = $alts->getImageAltTags();
-        $websites[] = [ 'url' => $url, 'alts' => $alts ];
-    }
+    use function WaughJ\TestHashItem\TestHashItemExists;
 
     $loader = new \Twig\Loader\FilesystemLoader( 'templates' );
     $twig = new \Twig\Environment
@@ -26,4 +17,36 @@
         $loader,
         [ 'cache' => '.cache' ]
     );
-    echo $twig->render( 'index.html.twig', [ 'websites' => $websites ] );
+
+    if ( TestHashItemExists( $_GET, 'url', null ) !== null )
+    {
+        $url = $_GET[ 'url' ];
+		$uri = new URI( $url );
+		if ( !$uri->getScheme() )
+		{
+			$uri->setScheme( 'https' );
+		}
+		$url = $uri->getUri();
+
+        if ( !filter_var( $url, FILTER_VALIDATE_URL ) )
+        {
+            echo $twig->render( 'invalid_url.html.twig', [ 'url' => $url ] );
+            return;
+        }
+
+        $links_list = new WebpageLinksList( $url, 250 );
+        $links_data = $links_list->getData();
+
+        $websites = [];
+        foreach ( $links_data as $link_url => $data )
+        {
+            $alts = new ImageAltTagList( $data->raw_body );
+            $alts = $alts->getImageAltTags();
+            $websites[] = [ 'url' => $link_url, 'alts' => $alts ];
+        }
+        echo $twig->render( 'results.html.twig', [ 'home' => $url, 'websites' => $websites ] );
+    }
+    else
+    {
+        echo $twig->render( 'form.html.twig', [] );
+    }
