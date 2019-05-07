0f260aef84882825534d72e69b00613dc5289270
Jaimeson Waugh <jaimeson@4cesi.com>
2019-05-07 16:29:32 -0700

    Work on Google recaptcha integration

--- src/index.php
+++ src/index.php
@@ -1,9 +1,20 @@
 <?php
 
+    const DEBUG = true;
+
     // Turn off all errors.
-    ini_set( 'display_errors', 0 );
-    ini_set( 'display_startup_errors', 0 );
-    error_reporting( E_NONE );
+    if ( DEBUG )
+    {
+        ini_set( 'display_errors', 1 );
+        ini_set( 'display_startup_errors', 1 );
+        error_reporting( E_ALL );
+    }
+    else
+    {
+        ini_set( 'display_errors', 0 );
+        ini_set( 'display_startup_errors', 0 );
+        error_reporting( E_NONE );
+    }
 
     require_once( 'vendor/autoload.php' );
 	use Enrise\Uri;
@@ -35,41 +46,49 @@
   }*/
 
 
-
     $loader = new \Twig\Loader\FilesystemLoader( '../src/templates' );
     $twig = new \Twig\Environment
     (
         $loader,
-        [ 'cache' => '../src/.cache' ]
+        []
     );
+
+    var_dump( $_POST );
 
     if ( TestHashItemExists( $_POST, 'url', null ) !== null )
     {
-        $url = $_POST[ 'url' ];
-		$uri = new URI( $url );
-		if ( !$uri->getScheme() )
-		{
-			$uri->setScheme( 'https' );
-		}
-		$url = $uri->getUri();
-
-        if ( !filter_var( $url, FILTER_VALIDATE_URL ) )
+        if ( TestHashItemExists( $_POST, 'recaptcha_response', null ) !== null )
         {
-            echo $twig->render( 'invalid_url.html.twig', [ 'url' => $url ] );
-            return;
+            $url = $_POST[ 'url' ];
+    		$uri = new URI( $url );
+    		if ( !$uri->getScheme() )
+    		{
+    			$uri->setScheme( 'https' );
+    		}
+    		$url = $uri->getUri();
+
+            if ( !filter_var( $url, FILTER_VALIDATE_URL ) )
+            {
+                echo $twig->render( 'invalid_url.html.twig', [ 'url' => $url ] );
+                return;
+            }
+
+            $links_list = new WebpageLinksList( $url, 250 );
+            $links_data = $links_list->getData();
+
+            $websites = [];
+            foreach ( $links_data as $link_url => $data )
+            {
+                $alts = new ImageAltTagList( $data->raw_body );
+                $alts = $alts->getImageAltTags();
+                $websites[] = [ 'url' => $link_url, 'alts' => $alts ];
+            }
+            echo $twig->render( 'results.html.twig', [ 'home' => $url, 'websites' => $websites ] );
         }
-
-        $links_list = new WebpageLinksList( $url, 250 );
-        $links_data = $links_list->getData();
-
-        $websites = [];
-        foreach ( $links_data as $link_url => $data )
+        else
         {
-            $alts = new ImageAltTagList( $data->raw_body );
-            $alts = $alts->getImageAltTags();
-            $websites[] = [ 'url' => $link_url, 'alts' => $alts ];
+            echo 'baaah';
         }
-        echo $twig->render( 'results.html.twig', [ 'home' => $url, 'websites' => $websites ] );
     }
     else
     {
